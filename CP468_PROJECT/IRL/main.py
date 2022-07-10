91076e668816697cb2444b48f04607459784f42f
hello world <42101911+mraa022@users.noreply.github.com>
2022-07-10 19:25:35 -0400

    fixed bugs in the irl module
    - there might be bugs still in the program
    - very inefficient, work on optimizing it

--- CP468_PROJECT/IRL/main.py
+++ CP468_PROJECT/IRL/main.py
@@ -30,71 +30,55 @@ def feature_expectations(trajectories,phi,gamma):
 def recover_r(phi,gamma):
     b = Board(10,0)
     epsilon = 0.01
-    trajectories = generate_trajectories()
     gamma = 0.9
 
-    
-    nonexpert_policies = [lambda state,symbol:random.choice([x for x in [(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)] if state.is_empty(x)])] # start with random policy
-    reward = lambda s:np.matmul(w,phi(s))
-    expert_feature_expectations = feature_expectations(trajectories,phi,gamma)
+    i = 0
+    trajectories = generate_trajectories()
+    mu_e = feature_expectations(trajectories,phi,gamma)
+    pi_o = lambda state,symbol: random.choice(state.possible_actions())
+    mu_o = feature_expectations(generate_trajectories(pi_o),phi,gamma)
+    policies = [pi_o]
+    none_expert_feature_expectations = [mu_o]
+    mu_bars = []
+    i+=1
+    for i in range(1,101):
+        if i==1:
+            mu_bar_o = deepcopy(mu_o)
+            # mu_bars.append(np.zeros(9,))
+            mu_bars.append(mu_bar_o)
+            w = mu_e - mu_o
+        else:
+            # mu_bar_prev = mu_bars[-1]
+            mu_bar_prev_prev = mu_bars[i-2]
+            # print(none_expert_feature_expectations,' f ')
+            # print(mu_bars)
+            # print()
+            mu_prev = none_expert_feature_expectations[i-1]
+            # print(mu_e,mu_bar_prev,mu_bar_prev_prev,mu_prev)
+            # mu_bar_prev = mu_bar_prev_prev + (((mu_prev-mu_bar_prev_prev).T @ (mu_e-mu_bar_prev_prev))/((mu_prev-mu_bar_prev_prev).T @ (mu_prev-mu_bar_prev_prev)))*(mu_prev-mu_bar_prev_prev)
 
-    none_expert_trajectories = generate_trajectories(nonexpert_policies)
-
-    nonexpert_feature_expectations  = none_expert_trajectories # mu_o
-    nonexpert_blended_feature_expectations = deepcopy(nonexpert_feature_expectations) # mu_bar_o
-    w = expert_feature_expectations - nonexpert_feature_expectations
-    # nonexpert_blended_feature_expectations = np.vstack((nonexpert_blended_feature_expectations,np.zeros(shape=(9,))))
-    for _ in range(10):
-        
-        policy = nonexpert_policies[-1]
-        trajectories = generate_trajectories(policy)
-        nonexpert_feature_expectations = np.vstack((nonexpert_feature_expectations,
-                                                        feature_expectations(trajectories,phi,gamma)))
-        # Use the projection method to find our current error
-        mu_e = expert_feature_expectations
-        mu_prev = nonexpert_feature_expectations[-1]
-
-        mu_prev_prev = nonexpert_feature_expectations[-2]
-        mu_bar_prev_prev = nonexpert_blended_feature_expectations[-2]
-        # print('ffff ',mu_prev_prev)
-        # The below finds the orthogonal projection of the expert's
-        # feature expectations onto the line through mu_prev and
-        # mu_prev_prev
-        mu_bar_prev = mu_bar_prev_prev + ((mu_prev - mu_bar_prev_prev.T @ (mu_e-mu_bar_prev_prev))/(mu_prev - mu_bar_prev_prev.T @ (mu_prev-mu_bar_prev_prev)))@(mu_prev-mu_bar_prev_prev)
-        # mu_bar_prev = mu_bar_prev_prev \
-        #     + (mu_prev - mu_bar_prev_prev).T \
-        #         @ (mu_e - mu_bar_prev_prev) \
-        #     / (mu_prev - mu_bar_prev_prev).T \
-        #         @ (mu_prev - mu_bar_prev_prev) \
-        #     * (mu_prev - mu_bar_prev_prev)
-
-        nonexpert_blended_feature_expectations = np.vstack(
-            (
-                nonexpert_blended_feature_expectations,
-                mu_bar_prev
-            )
-        )
-
-        w = mu_e - mu_bar_prev
-
+            # k = ((mu_prev-mu_bar_prev_prev).T @ (mu_e-mu_bar_prev_prev))/((mu_prev-mu_bar_prev_prev).T @ (mu_prev-mu_bar_prev_prev))
+            top_half = (mu_prev -mu_bar_prev_prev).T @ (mu_e-mu_bar_prev_prev)
+            bottom_half = (mu_prev -mu_bar_prev_prev).T @ (mu_prev-mu_bar_prev_prev)
+            mu_bar_prev = mu_bar_prev_prev  + (top_half/bottom_half)*(mu_prev-mu_bar_prev_prev)
+               
+            w = mu_e - mu_bar_prev
+            mu_bars.append(mu_bar_prev)
+        reward_function = lambda s: np.matmul(w,phi(s))
         # print(w)
-        # Lambda for the current reward estimate
-        # print(phi(b).shape,'ffff')
-        reward_function = lambda s: np.matmul(w.T, phi(s))
-        # reward_function = lambda s: print(phi(s))
+        Q_table = find_Q_table(Board(10,0),reward_function,gamma)
+        new_policy = new_policy = lambda s,symbol:found_policy(s,deepcopy(Q_table),epsilon) 
 
-        # Check the error of the current hyperplane, and break if we're close
-        # to the expert's feature expectations
-        t = np.linalg.norm(w)
-        print('FFFFFFF ',np.linalg.norm(mu_e-mu_bar_prev))
-        if t < epsilon:
+
+        policies.append(new_policy)
+        
+        none_expert_feature_expectations.append(feature_expectations(generate_trajectories(new_policy),phi,gamma))
+            
+        t = np.linalg.norm(mu_e-mu_bars[-1])
+        if t<0.5:
             break
-
-        # Compute a new optimal policy using the current reward function
-        Q_table = find_Q_table(b,reward_function,gamma)
-        new_policy = lambda s,symbol:found_policy(s,deepcopy(Q_table),epsilon)
-        nonexpert_policies.append(new_policy)
-    return w,nonexpert_policies[-1]
+        print(t)
+    return w,policies[-1]
 # def policy(state,symbol):
 #     possible_choices = []
 #     for x in 
